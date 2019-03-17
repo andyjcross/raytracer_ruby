ecb01c95b6b78523b4672189ee74dee6a054ba90
Andrew Cross <albany551@gmail.com>
2019-03-16 20:12:02 -0700

    Add lambertian shading support, and shadows

--- main.rb
+++ main.rb
@@ -6,25 +6,23 @@ require_relative "light"
 require_relative "image"
 
 objects = [
-  Sphere.new(Vector3.new(-3, -1, 3), 4, Color.new(0, 0, 255)), # blue
-  Sphere.new(Vector3.new(5, -1, 5), 1, Color.new(255, 0, 0)), # red
-  Sphere.new(Vector3.new(0, 3, 8), 3, Color.new(0, 255, 0)) # green
+  Sphere.new(Vector3.new(-3, -1, 3), 4, Color.new(0, 0, 1)), # blue
+  Sphere.new(Vector3.new(3, -2, 0), 1, Color.new(1, 0, 0)), # red
+  Sphere.new(Vector3.new(0, 3, 8), 3, Color.new(0, 1, 0)) # green
 ]
 
 lights = [
-  Light.new(Vector3.new(1, 8, 2), 5)
+  Light.new(Vector3.new(8, -1, 1), 1.0)
 ]
 
 eye_position = Vector3.new(0, 0, -10)
-
 window_width = 10
 window_height = 10
 
-image = Image.new(256, 256)
+image = Image.new(512, 512)
 
 image.width.times do |x|
   image.height.times do |y|
-
     pixel_x = ((x.to_f / image.width) * window_width) - (window_width.to_f / 2)
     pixel_y = -(((y.to_f / image.height) * window_height) - (window_height.to_f / 2))
     pixel_position = Vector3.new(pixel_x, pixel_y, 0)
@@ -45,10 +43,33 @@ image.width.times do |x|
     end
 
     if closest_intersection
-      color = closest_intersection[:object].color
+      surface_color = closest_intersection[:object].color
+
+      ambient_color = Color.new(1.0, 1.0, 1.0)
+      ambient_factor = 0.2
+
+      color = color + (surface_color * ambient_color * ambient_factor)
 
       lights.each do |light|
+        light_direction = (light.position - closest_intersection[:position]).normalized
+        light_color = Vector3.new(1.0, 1.0, 1.0)
+        diffuse_factor = 1.0
 
+        objects.each do |object|
+          shadow_ray = Ray.new(closest_intersection[:position] + (light_direction * 0.001), light.position)
+
+          shadow_intersection = object.intersect(shadow_ray)
+          if shadow_intersection && shadow_intersection[:distance] > 0.0
+            diffuse_factor = 0.0
+            break
+          end
+        end
+
+        cos_incident_angle = light_direction.dot_product(closest_intersection[:normal])
+        if cos_incident_angle > 0
+
+          color += (light_color * cos_incident_angle * surface_color * diffuse_factor)
+        end
       end
     end
 
--- sphere.rb
+++ sphere.rb
@@ -8,32 +8,16 @@ class Sphere
   end
 
   def intersect(ray)
-    # dst = ray.position - position
-    # b = 2.0 * dst.dot_product(ray.direction)
-    # c = dst.dot_product(dst) - (radius * radius)
-    # discr = b * b - 4.0 * c
-
-    # if discr >= 0
-    #   t0 = (-b - Math.sqrt(discr)) * 0.5
-    #   if t0 >= 0
-    #     intersection_position = ray.position + (ray.direction * t0)
-    #     normal = (intersection_position - position).normalized
-    #     return { distance: t0, normal: normal }
-    #   end
-    # end
-    # return nil
-
     dist = position - ray.position
     b = ray.direction.dot_product(dist)
     d = (b * b) - dist.dot_product(dist) + (radius * radius)
 
     return nil if d < 0.0
 
-    t0 = b - Math.sqrt(d)
+    t = b - Math.sqrt(d)
 
-    intersection_position = ray.position + (ray.direction * t0)
+    intersection_position = ray.position + (ray.direction * t)
     normal = (intersection_position - position).normalized
-
-    return { object: self, distance: t0, normal: normal }
+    return { object: self, distance: t, position: intersection_position, normal: normal }
   end
 end
